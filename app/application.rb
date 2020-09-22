a78bbedd2b85266e417efc2a9c99142787a78060
Keesha Hargrove <kharg14@gmail.com>
2020-09-22 03:12:09 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,9 @@
-class Application
+# class Application
 
-  def call(env)
-    resp = Rack::Response.new
-    resp.write "Hello, World"
-    resp.finish
-  end
+#   def call(env)
+#     resp = Rack::Response.new
+#     resp.write "Hello, World"
+#     resp.finish
+#   end
 
-end
+# end
